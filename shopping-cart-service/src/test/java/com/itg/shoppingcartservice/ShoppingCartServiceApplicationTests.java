1afa0a0569044f695955311feaa297779f9573c2
Serhatb0 <77425377+Serhatb0@users.noreply.github.com>
2024-09-04 19:48:54 +0300

    added git ignore

---
+++ shopping-cart-service/src/test/java/com/itg/shoppingcartservice/ShoppingCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itg.shoppingcartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
