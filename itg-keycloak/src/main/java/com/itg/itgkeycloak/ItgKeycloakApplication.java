e7604fbd946dfd4dab8b14f9c567f621bf5169c1
Serhatb0 <77425377+Serhatb0@users.noreply.github.com>
2024-09-14 13:37:06 +0300

    keyckoak

---
+++ itg-keycloak/src/main/java/com/itg/itgkeycloak/ItgKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.itg.itgkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItgKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItgKeycloakApplication.class, args);
+	}
+
+}
---
+++ itg-keycloak/src/test/java/com/itg/itgkeycloak/ItgKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itg.itgkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItgKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
