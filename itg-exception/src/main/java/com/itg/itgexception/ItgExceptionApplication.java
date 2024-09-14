3dc00875384421ac584ebc91d83ae6dd2d7d8e81
Serhatb0 <77425377+Serhatb0@users.noreply.github.com>
2024-09-14 14:26:05 +0300

    Exception Handling

---
+++ itg-exception/src/main/java/com/itg/itgexception/ItgExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.itg.itgexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItgExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItgExceptionApplication.class, args);
+    }
+
+}
---
+++ itg-exception/src/test/java/com/itg/itgexception/ItgExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itg.itgexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItgExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
